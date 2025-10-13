00963c0752eaba4de57536b8edb7d5a24cb45cc7
whiteflies <laura.lungum@gmail.com>
2025-10-13 22:29:32 +0100

    troubleshooting

--- compile-thesis.sh
+++ compile-thesis.sh
@@ -2,86 +2,113 @@
 # A script to compile the PhD Thesis - Krishna Kumar 
 # Distributed under GPLv2.0 License
 
-compile="compile";
-clean="clean";
+#!/usr/bin/env bash
+set -euo pipefail
 
-if test -z "$2"
-then
-if [ $1 = $clean ]; then
-	echo "Cleaning please wait ..."
-	rm -f *~
-	rm -rf *.aux
-	rm -rf *.bbl
-	rm -rf *.blg
-	rm -rf *.d
-	rm -rf *.fls
-	rm -rf *.ilg
-	rm -rf *.ind
-	rm -rf *.toc*
-	rm -rf *.lot*
-	rm -rf *.lof*
-	rm -rf *.log
-	rm -rf *.idx
-	rm -rf *.out*
-	rm -rf *.nlo
-	rm -rf *.nls
-	rm -rf $filename.pdf
-	rm -rf $filename.ps
-	rm -rf $filename.dvi
-	rm -rf *#* 
-	echo "Cleaning complete!"
-	exit
-else
-	echo "Shell script for compiling the PhD Thesis"
-	echo "Usage: sh ./compile-thesis.sh [OPTIONS] [filename]"
-	echo "[option]  compile: Compiles the PhD Thesis"
-	echo "[option]  clean: removes temporary files no filename required"
-	exit
-fi
+MAIN="${1:-thesis.tex}"
+ACTION="${2:-build}"
+
+cd "$(dirname "$0")"
+
+if [[ "$ACTION" == "clean" ]]; then
+  latexmk -C "$MAIN"
+  rm -f "${MAIN%.tex}.bbl" "${MAIN%.tex}.bcf" "${MAIN%.tex}.run.xml" || true
+  echo "Cleaned."
+  exit 0
 fi
 
-filename=$2;
+# Run pdflatex once to generate the .bcf file for biber
+pdflatex -interaction=nonstopmode -synctex=1 "$MAIN"
 
-if [ $1 = $clean ]; then
-	echo "Cleaning please wait ..."
-	rm -f *~
-	rm -rf *.aux
-	rm -rf *.bbl
-	rm -rf *.blg
-	rm -rf *.d
-	rm -rf *.fls
-	rm -rf *.ilg
-	rm -rf *.ind
-	rm -rf *.toc*
-	rm -rf *.lot*
-	rm -rf *.lof*
-	rm -rf *.log
-	rm -rf *.idx
-	rm -rf *.out*
-	rm -rf *.nlo
-	rm -rf *.nls
-	rm -rf $filename.pdf
-	rm -rf $filename.ps
-	rm -rf $filename.dvi
-	rm -rf *#* 
-	echo "Cleaning complete!"
-	exit
-elif [ $1 = $compile ]; then
-	echo "Compiling your PhD Thesis...please wait...!"
-	pdflatex -interaction=nonstopmode $filename.tex
-	bibtex $filename.aux 	
-	makeindex $filename.aux
-	makeindex $filename.idx
-	makeindex $filename.nlo -s nomencl.ist -o $filename.nls
-	pdflatex -interaction=nonstopmode $filename.tex
-	makeindex $filename.nlo -s nomencl.ist -o $filename.nls
-	pdflatex -interaction=nonstopmode $filename.tex
-	echo "Success!"
-	exit
-fi
+# Run biber for references
+biber "${MAIN%.tex}"
+
+# Run pdflatex two more times to resolve references
+pdflatex -interaction=nonstopmode -synctex=1 "$MAIN"
+pdflatex -interaction=nonstopmode -synctex=1 "$MAIN"
+
+echo "✅ Done! Output: ${MAIN%.tex}.pdf"
 
 
-if test -z "$3"
-then
-	exit
-fi
+
+
+# OLD version below
+
+
+#compile="compile";
+#clean="clean";
+
+#if test -z "$2"
+#then
+#if [ $1 = $clean ]; then
+#	rm -f *~
+#	rm -rf *.aux
+#	rm -rf *.bbl
+#	rm -rf *.fls
+#	rm -rf *.ilg
+#	rm -rf *.ind
+#	rm -rf *.toc*
+#	rm -rf *.lot*
+#	rm -rf *.lof*
+#	rm -rf *.log
+#	rm -rf *.idx
+#	rm -rf *.out*
+#	rm -rf *.nlo
+#	rm -rf *.nls
+#	rm -rf $filename.pdf
+#	rm -rf $filename.ps
+#	rm -rf $filename.dvi
+#	rm -rf *#* 
+#	echo "Cleaning complete!"
+#	exit
+#else
+#	echo "Shell script for compiling the PhD Thesis"
+#	echo "Usage: sh ./compile-thesis.sh [OPTIONS] [filename]"
+#	echo "[option]  compile: Compiles the PhD Thesis"
+#	echo "[option]  clean: removes temporary files no filename required"
+#	exit
+#fi
+#fi
+
+#filename=$2;
+
+#if [ $1 = $clean ]; then
+#	echo "Cleaning please wait ..."
+#	rm -f *~
+#	rm -rf *.aux
+#	rm -rf *.bbl
+#	rm -rf *.blg
+#	rm -rf *.d
+#	rm -rf *.fls
+#	rm -rf *.ilg
+#	rm -rf *.ind
+#	rm -rf *.toc*
+#	rm -rf *.lot*
+#	rm -rf *.lof*
+#	rm -rf *.log
+#	rm -rf *.idx
+#	rm -rf *.out*
+#	rm -rf *.nlo
+#	rm -rf $filename.pdf
+#	rm -rf $filename.dvi
+#	rm -rf *#* 
+#	echo "Cleaning complete!"
+#	exit
+##	echo "Compiling your PhD Thesis...please wait...!"
+#	pdflatex -interaction=nonstopmode $filename.tex
+#	bibtex $filename.aux 	
+#	makeindex $filename.aux
+#	makeindex $filename.idx
+#	makeindex $filename.nlo -s nomencl.ist -o $filename.nls
+#	pdflatex -interaction=nonstopmode $filename.tex
+#	makeindex $filename.nlo -s nomencl.ist -o $filename.nls
+#	pdflatex -interaction=nonstopmode $filename.tex
+#	echo "Success!"
+#	exit
+#fi
+
+
+#if test -z "$3"
+#then
+#	exit
+#fi
